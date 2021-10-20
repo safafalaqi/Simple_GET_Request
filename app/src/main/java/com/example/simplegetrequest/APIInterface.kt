1cc84556f8be05e45b854ee0fb0bfde75eaa0ff9
Safa <sfalaqi@gmail.com>
2021-10-20 14:33:06 +0300

    Simple POST Request

--- app/src/main/java/com/example/simplegetrequest/APIInterface.kt
+++ app/src/main/java/com/example/simplegetrequest/APIInterface.kt
@@ -1,10 +1,15 @@
 package com.example.simplegetrequest
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface APIInterface {
 
-    @GET("people/")
+    @GET("custom-people/")
   fun getUsersInfo(): Call<People?>?
+
+    @POST("custom-people/")
+    fun addUsersInfo(@Body requestBody: PersonListItem?): Call<PersonListItem>
 
 }
--- app/src/main/java/com/example/simplegetrequest/MainActivity.kt
+++ app/src/main/java/com/example/simplegetrequest/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.simplegetrequest
 
+import android.app.AlertDialog
 import android.app.ProgressDialog
+import android.content.DialogInterface
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.InputType
+import android.view.View
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.simplegetrequest.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,9 +34,7 @@ class MainActivity : AppCompatActivity() {
         createApiInterface()
     }
 
-    fun createApiInterface()
-    {
-
+    fun createApiInterface() {
         //show progress Dialog
         val progressDialog = ProgressDialog(this@MainActivity)
         progressDialog.setMessage("Please wait")
@@ -56,11 +65,63 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
-
     private fun setRV(data: People) {
         myRv = findViewById(R.id.rvPeople)
         rvAdapter =RVAdapter(data, this)
         myRv.adapter = rvAdapter
         myRv.layoutManager = LinearLayoutManager(applicationContext)
     }
+
+    fun customAlert(v: View) {
+
+            // first we create a variable to hold an AlertDialog builder
+            val dialogBuilder =  AlertDialog.Builder(this)
+            val editText=EditText(this)
+            editText.setInputType(InputType.TYPE_CLASS_TEXT)
+            dialogBuilder.setView(editText)
+
+            dialogBuilder.setPositiveButton("Add", DialogInterface.OnClickListener {
+                        dialog, id -> addUser(editText.text.toString())
+                })
+                // negative button text and action
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+                        dialog, id -> dialog.cancel()
+                })
+            // create dialog box
+            val alert = dialogBuilder.create()
+            // set title for alert dialog box
+            alert.setTitle("Enter a person name")
+            //alert.setView(editText)
+            // show alert dialog
+            alert.show()
+    }
+    fun addUser(name:String){
+
+        //check if user inputs are not empty
+        if(name.isNotEmpty()) {
+            val apiInterface = APIClient().getClient()?.create(APIInterface::class.java)
+            val user = PersonListItem(name)
+            val call: Call<PersonListItem> = apiInterface!!.addUsersInfo(user)
+
+            call?.enqueue(object : Callback<PersonListItem?> {
+                override fun onResponse(
+                    call: Call<PersonListItem?>?,
+                    response: Response<PersonListItem?>
+                ) {
+
+                    Toast.makeText(applicationContext, "Save Success!", Toast.LENGTH_SHORT).show()
+                    createApiInterface()
+                }
+                override fun onFailure(call: Call<PersonListItem?>, t: Throwable) {
+                    Toast.makeText(applicationContext, "Unable to add person.", Toast.LENGTH_SHORT)
+                        .show()
+                    call.cancel()
+                }
+            })
+        }
+        else {
+            Toast.makeText(applicationContext, "Please do not leave it empty!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
